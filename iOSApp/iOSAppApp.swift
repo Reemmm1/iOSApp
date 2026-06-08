f341366398b7d3c3701a3b77de86d3600e3d7ff4
Reem El Machtoub <reem.elmachtoub@monash.edu>
2026-06-08 12:52:18 +1000

    Initial Commit

---
+++ iOSApp/iOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAppApp.swift
+//  iOSApp
+//
+//  Created by Reem El Machtoub on 8/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct iOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
